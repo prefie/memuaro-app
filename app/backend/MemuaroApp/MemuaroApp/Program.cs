96fc3d58fa3a47d1849776f32746ed874b332e81
prefie <57269396+prefie@users.noreply.github.com>
2022-10-08 23:07:49 +0500

    Основа для аутентификации через гугл (#1)

--- app/backend/MemuaroApp/MemuaroApp/Program.cs
+++ app/backend/MemuaroApp/MemuaroApp/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthentication()
+    .AddGoogle(options =>
+    {
+        options.ClientId = Environment.GetEnvironmentVariable("ClientId") ?? string.Empty;
+        options.ClientSecret = Environment.GetEnvironmentVariable("ClientSecret") ?? string.Empty;
+    });
 
 var app = builder.Build();
 
