caa7bdfd0ff20bf4c5776660f023c79bf8179f81
Darya <dashav1605@mail.ru>
2022-10-21 01:40:37 +0500

    Create auth page with google auth button

--- app/frontend/src/app/pages/auth/auth-page.module.ts
+++ app/frontend/src/app/pages/auth/auth-page.module.ts
@@ -1,15 +1,15 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {AuthPageComponent} from './auth-page.component';
-import {NzLayoutModule} from 'ng-zorro-antd/layout';
-import {NzPageHeaderModule} from 'ng-zorro-antd/page-header';
-import {AuthPageRoutingModule} from './auth-page-routing.module';
-import {NzIconModule} from 'ng-zorro-antd/icon';
-import {NzButtonModule} from 'ng-zorro-antd/button';
-import {SvgIconModule} from '../../components/svg-icon/svg-icon.module';
-import {FormsModule} from '@angular/forms';
-import {GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule} from '@abacritt/angularx-social-login';
-import {CLIENT_ID} from './auth-page.models';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AuthPageComponent } from './auth-page.component';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { AuthPageRoutingModule } from './auth-page-routing.module';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { SvgIconModule } from '../../components/svg-icon/svg-icon.module';
+import { FormsModule } from '@angular/forms';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from '@abacritt/angularx-social-login';
+import { CLIENT_ID } from './auth-page.models';
 
 @NgModule({
   declarations: [
